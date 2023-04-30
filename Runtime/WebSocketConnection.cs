01b0e5e2f8a490030b36f9189050a5ff500139c0
Mike Schweitzer <mschweitzer@gmail.com>
2023-04-30 15:45:51 -0700

    Message queue pump and API clarity improvements

--- Runtime/WebSocketConnection.cs
+++ Runtime/WebSocketConnection.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using NativeWebSocket;
 using UnityEngine;
 
 namespace Mikerochip.WebSocket
 {
-    public enum WebSocketPendingState
+    public enum WebSocketDesiredState
     {
         None,
         Connect,
@@ -19,8 +21,10 @@ namespace Mikerochip.WebSocket
         Connecting,
         Connected,
         Disconnecting,
-        Disconnected,
-        // see LastError property for the error message
+        // clean shutdown will result in this state
+        Closed,
+        // error will result in this state
+        // see LastErrorMessage property for the error message
         Error,
     }
 
@@ -35,8 +39,8 @@ namespace Mikerochip.WebSocket
 
     public class WebSocketConnection : MonoBehaviour
     {
-        // set these before you connect
-        #region Desired Properties
+        // set and check what state you requested 
+        #region Desired State Properties
         public string DesiredUrl
         {
             get => DesiredConfig?.Url;
@@ -47,35 +51,39 @@ namespace Mikerochip.WebSocket
             }
         }
         public WebSocketConfig DesiredConfig { get; set; } = new WebSocketConfig();
+        public WebSocketDesiredState DesiredState { get; private set; }
         #endregion
         
-        // read-only properties to query current state
-        #region Active Properties
-        public WebSocketPendingState PendingState { get; private set; }
+        // check the current state of the connection
+        #region Current State Properties
+        public string Url => Config?.Url;
         public WebSocketConfig Config { get; private set; }
         public WebSocketState State { get; private set; }
         
-        public string LastError { get; private set; }
-        public byte[] LastIncomingMessage { get; private set; }
-        public string LastIncomingMessageString => BytesToString(LastIncomingMessage);
+        public bool IsConnecting => DesiredState == WebSocketDesiredState.Connect || State == WebSocketState.Connecting;
+        public bool IsDisconnecting => DesiredState == WebSocketDesiredState.Disconnect || State == WebSocketState.Disconnecting;
         
-        // Don't use these unless you really need to manipulate the message Queues directly.
-        // I recommend casting if you want to alter them.
+        public string ErrorMessage { get; private set; }
+        public byte[] LastIncomingMessageBytes => _incomingMessages.LastOrDefault();
+        public string LastIncomingMessageString => BytesToString(LastIncomingMessageBytes);
+        
+        // You probably don't need these and should use the methods instead. These are only here
+        // if you really want to manipulate the message Queues directly, for some reason.
         public IEnumerable<byte[]> IncomingMessages => _incomingMessages;
         public IEnumerable<byte[]> OutgoingMessages => _outgoingMessages;
         #endregion
         
-        // for your convenience
+        // optional events, raised when current state changes
         #region Public Events
         public event Action<WebSocketConnection> Connected;
         public event Action<WebSocketConnection> Disconnected;
-        // LastIncomingMessage if you need to know what was received
+        // check LastIncomingMessage* to know what was received
         public event Action<WebSocketConnection> MessageReceived;
-        public event Action<WebSocketConnection> Error;
         #endregion
 
         #region Private Fields
         private NativeWebSocket.WebSocket _webSocket;
+        private Task _connectTask;
         private readonly Queue<byte[]> _incomingMessages = new Queue<byte[]>();
         private readonly Queue<byte[]> _outgoingMessages = new Queue<byte[]>();
         #endregion
@@ -86,12 +94,18 @@ namespace Mikerochip.WebSocket
             if (url != null)
                 DesiredUrl = url;
 
-            PendingState = WebSocketPendingState.Connect;
+            DesiredState = WebSocketDesiredState.Connect;
         }
 
         public void Disconnect()
         {
-            PendingState = WebSocketPendingState.Disconnect;
+            if (DesiredState == WebSocketDesiredState.Connect)
+            {
+                DesiredState = WebSocketDesiredState.None;
+                return;
+            }
+            
+            DesiredState = WebSocketDesiredState.Disconnect;
         }
 
         public void AddOutgoingMessage(string str)
@@ -117,12 +131,7 @@ namespace Mikerochip.WebSocket
 
         public bool TryRemoveIncomingMessage(out byte[] bytes)
         {
-            if (!_incomingMessages.TryDequeue(out bytes))
-                return false;
-
-            if (_incomingMessages.Count == 0)
-                LastIncomingMessage = null;
-            return true;
+            return _incomingMessages.TryDequeue(out bytes);
         }
 
         public static byte[] StringToBytes(string str) =>
@@ -131,11 +140,118 @@ namespace Mikerochip.WebSocket
         public static string BytesToString(byte[] bytes) =>
             bytes == null ? null : Encoding.UTF8.GetString(bytes);
         #endregion
-
-        #region Private Methods
-        private WebSocketConfig DeepCopy(WebSocketConfig src)
+        
+        #region Unity Methods
+        private async void Awake()
         {
-            var dst = new WebSocketConfig
+            await Task.WhenAll(ManageStateAsync(), ReceiveAsync(), SendAsync());
+        }
+        #endregion
+
+        #region Internal Async Management
+        private async Task ManageStateAsync()
+        {
+            while (true)
+            {
+                // handle active states first
+                if (State == WebSocketState.Disconnecting)
+                {
+                    await ShutdownWebSocketAsync();
+                    State = ErrorMessage == null ? WebSocketState.Closed : WebSocketState.Error;
+                    Disconnected?.Invoke(this);
+                }
+                
+                // process desired states now
+                if (DesiredState == WebSocketDesiredState.Connect)
+                {
+                    DesiredState = WebSocketDesiredState.None;
+                    
+                    await ShutdownWebSocketAsync();
+                    
+                    State = WebSocketState.Connecting;
+                    InitializeWebSocket();
+                    _connectTask = _webSocket!.Connect();
+                }
+                else if (DesiredState == WebSocketDesiredState.Disconnect)
+                {
+                    DesiredState = WebSocketDesiredState.None;
+
+                    if (_webSocket != null)
+                    {
+                        _webSocket.CancelConnection();
+                        await _webSocket.Close();
+                    }
+                }
+
+                await Task.Yield();
+            }
+        }
+
+        private async Task ReceiveAsync()
+        {
+            while (true)
+            {
+                if (_connectTask != null)
+                    await _connectTask;
+
+                await Task.Yield();
+            }
+        }
+
+        private async Task SendAsync()
+        {
+            while (true)
+            {
+                while (_webSocket?.State == NativeWebSocket.WebSocketState.Open &&
+                       _outgoingMessages.TryDequeue(out var bytes))
+                {
+                    await _webSocket.Send(bytes);
+                }
+
+                await Task.Yield();
+            }
+        }
+        #endregion
+
+        #region Internal WebSocket Management
+        private void InitializeWebSocket()
+        {
+            Config = DeepCopy(DesiredConfig);
+
+            ErrorMessage = null;
+            
+            _webSocket = new NativeWebSocket.WebSocket(Config.Url, Config.Subprotocols, Config.Headers);
+            _webSocket.OnOpen += WebSocketOnOpen;
+            _webSocket.OnMessage += WebSocketOnMessage;
+            _webSocket.OnClose += WebSocketOnClose;
+            _webSocket.OnError += WebSocketOnError;
+        }
+
+        private async Task ShutdownWebSocketAsync()
+        {
+            if (_webSocket == null)
+                return;
+            
+            _webSocket.CancelConnection();
+            await _connectTask;
+            _connectTask = null;
+            
+            _incomingMessages.Clear();
+            _outgoingMessages.Clear();
+            
+            _webSocket.OnOpen -= WebSocketOnOpen;
+            _webSocket.OnMessage -= WebSocketOnMessage;
+            _webSocket.OnClose -= WebSocketOnClose;
+            _webSocket.OnError -= WebSocketOnError;
+            _webSocket = null;
+        }
+
+        private static WebSocketConfig DeepCopy(WebSocketConfig src)
+        {
+            if (src == null)
+                return new WebSocketConfig();
+            
+            return new WebSocketConfig
             {
                 Url = src.Url,
                 Subprotocols = src.Subprotocols?.ToList(),
@@ -143,7 +259,29 @@ namespace Mikerochip.WebSocket
                 MaxReceiveBytes = src.MaxReceiveBytes,
                 MaxSendBytes = src.MaxSendBytes,
             };
-            return dst;
+        }
+
+        private void WebSocketOnOpen()
+        {
+            State = WebSocketState.Connected;
+            Connected?.Invoke(this);
+        }
+
+        private void WebSocketOnMessage(byte[] data)
+        {
+            _incomingMessages.Enqueue(data);
+            MessageReceived?.Invoke(this);
+        }
+
+        private void WebSocketOnClose(WebSocketCloseCode closeCode)
+        {
+            State = WebSocketState.Disconnecting;
+        }
+
+        private void WebSocketOnError(string errorMsg)
+        {
+            State = WebSocketState.Disconnecting;
+            ErrorMessage = errorMsg;
         }
         #endregion
     }
