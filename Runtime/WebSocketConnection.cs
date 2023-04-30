76869e1af3256cc0d7cb362d3f3fc1e53146f07a
Mike Schweitzer <mschweitzer@gmail.com>
2023-04-30 00:32:48 -0700

    Flesh out message API

--- Runtime/WebSocketConnection.cs
+++ Runtime/WebSocketConnection.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using UnityEngine;
 
 namespace Mikerochip.WebSocket
@@ -19,6 +20,7 @@ namespace Mikerochip.WebSocket
         Connected,
         Disconnecting,
         Disconnected,
+        // see LastError property for the error message
         Error,
     }
 
@@ -33,6 +35,7 @@ namespace Mikerochip.WebSocket
 
     public class WebSocketConnection : MonoBehaviour
     {
+        // set these before you connect
         #region Desired Properties
         public string DesiredUrl
         {
@@ -46,26 +49,38 @@ namespace Mikerochip.WebSocket
         public WebSocketConfig DesiredConfig { get; set; } = new WebSocketConfig();
         #endregion
         
+        // read-only properties to query current state
         #region Active Properties
         public WebSocketPendingState PendingState { get; private set; }
         public WebSocketConfig Config { get; private set; }
         public WebSocketState State { get; private set; }
-        public Queue<byte[]> IncomingMessages { get; private set; }
+        
+        public string LastError { get; private set; }
+        public byte[] LastIncomingMessage { get; private set; }
+        public string LastIncomingMessageString => BytesToString(LastIncomingMessage);
+        
+        // Don't use these unless you really need to manipulate the message Queues directly.
+        // I recommend casting if you want to alter them.
+        public IEnumerable<byte[]> IncomingMessages => _incomingMessages;
+        public IEnumerable<byte[]> OutgoingMessages => _outgoingMessages;
         #endregion
         
+        // for your convenience
         #region Public Events
         public event Action<WebSocketConnection> Connected;
         public event Action<WebSocketConnection> Disconnected;
+        // LastIncomingMessage if you need to know what was received
         public event Action<WebSocketConnection> MessageReceived;
         public event Action<WebSocketConnection> Error;
         #endregion
 
         #region Private Fields
         private NativeWebSocket.WebSocket _webSocket;
-        private Queue<byte[]> _outgoingMessages = new Queue<byte[]>();
+        private readonly Queue<byte[]> _incomingMessages = new Queue<byte[]>();
+        private readonly Queue<byte[]> _outgoingMessages = new Queue<byte[]>();
         #endregion
 
-        #region Public Methods
+        #region Public API
         public void Connect(string url = null)
         {
             if (url != null)
@@ -78,23 +93,43 @@ namespace Mikerochip.WebSocket
         {
             PendingState = WebSocketPendingState.Disconnect;
         }
+
+        public void AddOutgoingMessage(string str)
+        {
+            var bytes = StringToBytes(str);
+            _outgoingMessages.Enqueue(bytes);
+        }
+
+        public void AddOutgoingMessage(byte[] bytes)
+        {
+            _outgoingMessages.Enqueue(bytes);
+        }
+
+        public bool TryRemoveIncomingMessage(out string str)
+        {
+            str = null;
+            if (!TryRemoveIncomingMessage(out byte[] bytes))
+                return false;
+            
+            str = BytesToString(bytes);
+            return true;
+        }
+
+        public bool TryRemoveIncomingMessage(out byte[] bytes)
+        {
+            if (!_incomingMessages.TryDequeue(out bytes))
+                return false;
+
+            if (_incomingMessages.Count == 0)
+                LastIncomingMessage = null;
+            return true;
+        }
+
+        public static byte[] StringToBytes(string str) =>
+            str == null ? null : Encoding.UTF8.GetBytes(str);
         
-        public void AddOutgoingMessage(string message)
-        {}
-        public void AddOutgoingMessage(byte[] message)
-        {}
-
-        public bool TryTakeReceivedMessage(out string message)
-        {
-            message = null;
-            return true;
-        }
-
-        public bool TryTakeReceivedMessage(out byte[] message)
-        {
-            message = null;
-            return true;
-        }
+        public static string BytesToString(byte[] bytes) =>
+            bytes == null ? null : Encoding.UTF8.GetString(bytes);
         #endregion
 
         #region Private Methods
